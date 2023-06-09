04abac51b967e020eeb3a0c7e38e17ad14da00ea
Femi Bolaji <bolajifemi28@gmail.com>
2023-06-09 16:19:40 +0100

    first commit

---
+++ commands.sh
@@ -0,0 +1,9 @@
+pnpm init
+tsc --init
+
+add src/index.ts
+
+pnpm add -D tsup   
+pnpm add vitest
+pnpm run build
+pnpm test
---
+++ src/index.test.ts
@@ -0,0 +1,7 @@
+import { describe, expect, it } from "vitest";
+
+describe("stuff here", ()=>{
+    it("should pass the CI", ()=>{
+        expect(1).toBe(1)
+    })
+})

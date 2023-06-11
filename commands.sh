fd8b2ea02aa07d82c8bd1b905390c3a54c015f86
Femi Bolaji <bolajifemi28@gmail.com>
2023-06-11 18:02:38 +0100

    changesets

--- commands.sh
+++ commands.sh
@@ -9,3 +9,5 @@ pnpm add @changesets/cli -D
 pnpm changeset init 
 pnpm run build
 pnpm test
+
+# You have to configure your repository - Settings -> Action -> General -> Workflow permissions and choose read and write permissions

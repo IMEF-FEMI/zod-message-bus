4a8d89e9786d9b54c1d8ddfef5f88feaba14b221
Femi Bolaji <bolajifemi28@gmail.com>
2023-06-11 18:26:12 +0100

    changesets

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,24 @@
 # zod-message-bus
 
+## 0.0.4
+
+### Patch Changes
+
+- updated .npmignore to add the following
+
+  src
+  .changeset
+  .github
+  pnpm-lock.yaml
+  tsconfig.json
+  vite.config.ts
+
+## 0.0.3
+
+### Patch Changes
+
+- fixed issue with dist folder
+
 ## 0.0.2
 
 ### Patch Changes
--- commands.sh
+++ commands.sh
@@ -9,5 +9,7 @@ pnpm add @changesets/cli -D
 pnpm changeset init 
 pnpm run build
 pnpm test
-
+add .npmignore
+pnpm changeset #to add new release changeset (basically a readme file added to the previous )
+pnpm changeset version
 # You have to configure your repository - Settings -> Action -> General -> Workflow permissions and choose read and write permissions
